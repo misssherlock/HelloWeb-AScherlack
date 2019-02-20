5852f68528027b069a5a19f74b85630ef3748e8f
Anna Scherlack <anna.scherlack@htl-salzburg.ac.at>
2019-02-20 16:40:32 +0100

    initial commit

---
+++ Assets/Scripts/Buttoninteraction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttoninteraction : MonoBehaviour {
+
+	// Use this for initialization
+	public void PrintHelloWeb () {
+        Debug.Log("Hello Web");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
