c1bea9495da057f20fd450cd526f7361582fe6f1
Anna Scherlack <anna.scherlack@htl-salzburg.ac.at>
2019-03-06 15:13:17 +0100

    SceneLoader

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+	public void LoadScene1()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void LoadScene2()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+
+}
+
